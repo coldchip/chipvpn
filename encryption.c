9b834c8f95788081257df5700f31fa2a3ce7b82d
Ryan <growwick@gmail.com>
2021-03-22 18:22:53 +0800

    removed encryption

--- encryption.c
+++ encryption.c
@@ -10,21 +10,25 @@ void chip_encrypt_set_key(EncryptCTX *ctx, char *key) {
 }
 
 void chip_encrypt_buf(EncryptCTX *ctx, char *data, int length) {
+	/*
 	uint8_t iv[16] = {
 		0x43, 0x37, 0x74, 0x3f, 0x0b, 0x33, 0x4e, 0x9c, 
 		0x7a, 0x5d, 0x26, 0x9a, 0x8d, 0xd2, 0x7b, 0x9d
 	};
 	AES_ctx_set_iv(&ctx->aes_ctx, iv);
 	AES_CTR_xcrypt_buffer(&ctx->aes_ctx, (uint8_t*)data, length);
+	*/
 }
 
 void chip_decrypt_buf(EncryptCTX *ctx, char *data, int length) {
+	/*
 	uint8_t iv[16] = {
 		0x43, 0x37, 0x74, 0x3f, 0x0b, 0x33, 0x4e, 0x9c, 
 		0x7a, 0x5d, 0x26, 0x9a, 0x8d, 0xd2, 0x7b, 0x9d
 	};
 	AES_ctx_set_iv(&ctx->aes_ctx, iv);
 	AES_CTR_xcrypt_buffer(&ctx->aes_ctx, (uint8_t*)data, length);
+	*/
 }
 
 void chip_decrypt_free(EncryptCTX *ctx) {
