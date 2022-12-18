bcd64e78cb9f89586906deeb6549b9dc15f60b1f
Ryan Loh <growwick@gmail.com>
2022-12-19 03:51:02 +0800

    cleanup codes

--- src/chipvpn.h
+++ src/chipvpn.h
@@ -22,7 +22,7 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-#define CHIPVPN_VERSION 10000065L
+#define CHIPVPN_VERSION 10000070L
 
 char              *chipvpn_read_file(const char *file);
 bool               chipvpn_get_gateway(struct in_addr *addr);
